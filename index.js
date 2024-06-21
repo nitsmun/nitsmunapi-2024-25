4db693784a168ac351ad30090d38d95534d9f322
nitsmun <nitsmun@nits.ac.in>
2024-06-21 20:06:07 +0530

    change put to patch method for update

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ import dotEnv from "dotenv";
 import cors from "cors";
 import * as controllers from "./src/controllers/index.js";
 
+
 dotEnv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origins: [`${process.env.ADMIN}`, `${process.env.FRONTEND}`] }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' }));
 app.get('/', (req, res) => {
     res.status(200).send(`Server running at port ${process.env.PORT}`);
 });
 
 app.post('/blog/add', controllers.blogs.create);
 app.get('/blog/read', controllers.blogs.read);
-app.put('/blog/update/:id', controllers.blogs.update);
+app.patch('/blog/update/:id', controllers.blogs.update);
 app.delete('/blog/delete/:id', controllers.blogs.del);
 
 app.listen(process.env.PORT, (req, res) => {
