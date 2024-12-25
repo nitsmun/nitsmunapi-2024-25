d0c180d488fde8abc70a7daa17d47b32d1d2d05e
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2024-12-25 11:41:34 +0530

    Added Update.ts

---
+++ src/controllers/participant/update.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+const updateParticipant = async(req: Request, res : Response) => {
+    const { id, name, email, institute, paymentProof, choice1, choice2, choice3, allotted, portfolio } = req.body;
+
+    try {
+        if(!id) {
+            return res.status(400).json({error : "No ID Found"}).send("Participant Updated Successfully");
+        }
+    
+        const updatedParticipant = await prisma.participants.update({
+            where: {
+                id : String(id),
+            },
+            data : {
+                ...(name && { name }),
+                ...(email && { email }),
+                ...(institute && { institute }),
+                ...(paymentProof && { paymentProof }),
+                ...(choice1 && { choice1 }),
+                ...(choice2 && { choice2 }),
+                ...(choice3 && { choice3 }),
+                ...(allotted && { allotted }),
+                ...(portfolio && { portfolio })
+            }
+        });
+    
+        return res.status(200).json(updatedParticipant);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error : "Partcipant not updated"}).send("Participant not updated");
+    }
+    
+
+}
